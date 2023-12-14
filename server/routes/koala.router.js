8918885756438be9219174e68128b0e119c9fdd0
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2023-12-14 17:35:31 -0600

    updated HTTPs

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,7 +1,8 @@
 // express constants
 const express = require("express");
+
 const koalaRouter = express.Router();
-const express = require("express");
+const router = express.Router();
 
 // DB CONNECTION
 // Import pool
@@ -50,38 +51,40 @@ router.get("/", (req, res) => {
 // POST
 // making a newKoala
 router.post("/", (req, res) => {
-  console.log("req.body", req.body);
+ // console.log("req.body", req.body);
 
-  const newKoala = req.body; // newKoala
+//   const newKoala = req.body; 
+//   // newKoala
 
-  // Sending data to DB
-  // ! Querytext
-  const queryText = `
-    INSERT INTO "koalas" ("name", "gender", "ready_to_transfer", "notes")
-    VALUES
-        ($1, $2, $3, $4);
-    `;
+//   // Sending data to DB
+//   // ! Querytext
+//   const queryText = `
+//     INSERT INTO "koalas" ("name", "gender", "ready_to_transfer", "notes")
+//     VALUES
+//         ($1, $2, $3, $4);
+//     `;
 
-  let queryParams = [
-    koalas.name,
-    koalas.gender,
-    koalas.ready_to_transfer,
-    koalas.notes,
-  ];
-  console.log("QueryText:", queryText);
+//   let queryParams = [
+//     newKoala.name,
+//     newKoala.gender,
+//     newKoala.ready_to_transfer,
+//     newKoala.notes,
+//   ];
+//   console.log("QueryText:", queryText);
 
-  pool
-    .query(queryText, queryParams)
-    .then((result) => {
-      console.log("QueryText:", queryText);
+//   pool
+    // .query(queryText, queryParams)
+    // .then((result) => {
+    //   console.log("QueryText:", queryText);
       res.sendStatus(201);
     })
-    .catch((error) => {
-      console.log("Woops, error making query: ", queryText);
-      console.error(error);
-      res.sendStatus(500);
-    });
-});
+// })
+//     .catch((error) => {
+//       console.log("Woops, error making query: ", queryText);
+//       console.error(error);
+//       res.sendStatus(500);
+//     });
+// });
 
 // PUT
 
@@ -103,7 +106,27 @@ router.put("/:id", (req, res) => {
 });
 
 // DELETE
-// no not yet
-// no
+//
+// Request must include a parameter indicating what book to update - the id
+router.delete("/:id", (req, res) => {
+  // Accessing the ID directly from req.params
+  // No need to assign it to another variable like reqId
+  let koalaId = req.params.id;
+
+  // SQL query to delete the book with the specified ID
+  let sqlText = "DELETE FROM koalas WHERE id=$1;";
+
+  // Executing the query using the pool object
+  pool
+    .query(sqlText, [koalaId])
+    .then((result) => {
+      console.log("koala ElImInAtEd");
+      res.sendStatus(200); // Send success status
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Send error status if there's a problem
+    });
+});
 
 module.exports = koalaRouter;
