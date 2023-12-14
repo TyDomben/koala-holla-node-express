b399ab6f24a6ea3d7bcd6bff5711b7c3a104d6a7
Dustin Hagstrom <dustnhgstrm@gmail.com>
2023-12-14 15:01:02 -0600

    wrote GET, POST, UPDATE queries and put them into 'database.sql.' Tested them with postico and postgresql.

--- DATABASE.sql
+++ DATABASE.sql
@@ -1,25 +1,35 @@
--- 
--- 
--- 
--- 
+-- Drop Table to reset data values
 DROP TABLE IF EXISTS "koalas";
 
+-- Create Table 'koalas'
 CREATE TABLE "koalas" (
 "id" SERIAL PRIMARY KEY,
-"name" VARCHAR NOT NULL,
-"gender" VARCHAR (5),
+"name" VARCHAR(20) NOT NULL,
+"gender" VARCHAR(5),
 "age" INTEGER,
-"ready_to_transfer" BOOLEAN, 
-"notes" VARCHAR (200));
+"ready_to_transfer" BOOL DEFAULT FALSE, 
+"notes" VARCHAR(200));
 
+-- Insert dummy data
 INSERT INTO "koalas"
-("name",	"gender",	"age",	"ready_to_transfer",	"notes")
+("name", "gender", "age", "ready_to_transfer", "notes")
 VALUES
+('Scotty', 'M', 4, 'Y', 'Born in Guatemala.'),
+('Jean', 'F', 5, 'Y', 'Allergic to lots of lava.'),
+('Ororo', 'F', 7, 'N', 'Loves listening to Paula (Abdul).'),
+('K''Leaf',	'NB', 15, 'N', 'Never refuses a treat.'),
+('Charlie',	'M', 9, 'Y', 'Favorite band is Nirvana.'),
+('Betsy', 'F', 4, 'Y', 'Has a pet iguana.');
 
-('Scotty',	'M',	4,	'Y',	'Born in Guatemala'),
-('Jean',	'F',	5,	'Y',	'Allergic to lots of lava'),
-('Ororo',	'F',	7,	'N',	'Loves listening to Paula (Abdul)'),
-('K''Leaf',	'NB',	15,	'N',	'Never refuses a treat.'),
-('Charlie',	'M',	9,	'Y',	'Favorite band is Nirvana'),
-('Betsy',	'F',	4,	'Y',	'Has a pet iguana');
+-- GET all data
+SELECT * FROM "koalas";
 
+-- POST to create a koala
+INSERT INTO "koalas"
+("name", "gender", "age", "ready_to_transfer", "notes")
+VALUES
+($1, $2, $3, $4, $5);
+
+-- UPDATE "ready_to_transfer" to True
+UPDATE "koalas" SET "ready_to_transfer" = TRUE
+WHERE "id" = $1;
