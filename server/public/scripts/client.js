4d6362502f0d08c1fb595ea8186f459e0068b6eb
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2023-12-14 19:09:25 -0600

    Added a PUT function also played around with the POST still comes up with errors, also added a submit function, not sure if its all correct

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,35 +7,93 @@ function getKoalas(){
 } // end getKoalas
 
 
-function clearForm(){
-  document.querySelector('#nameIn').value = '';
-  document.querySelector('#ageIn').value = '';
-  document.querySelector('#genderIn').value = '';
-  document.querySelector('#readyForTransferIn').value = '';
-  document.querySelector('#notesIn').value = '';
+function addKoala(event) {
+  event.preventDefault();
+
+  console.log("Submit button clicked.");
+  let koala = {};
+  koala.name = document.getElementById("nameIn").value;
+  koala.age = document.getElementById("ageIn").value;
+  koala.gender = document.getElementById("genderIn").value;
+  koala.readyForTransfer = document.getElementById("readyForTransferIn").value;
+  koala.notes = document.getElementById("notesIn").value;
+  saveKoala(koala);
 }
 
-function saveKoala(event){
-  event.preventDefault();
-  console.log( 'in saveKoala' );
-  // axios call to server to get koalas
-  //made this an object to create request body
-  let koalaObject ={
-    name: document.querySelector('#nameIn').value,
-    age: document.querySelector('#ageIn').value,
-    gender: document.querySelector('#genderIn').value,
-    readyForTransfer: document.querySelector('#readyForTransferIn').value,
-    notes: document.querySelector('#notesIn').value
-  }
-  //send the new koala data to the server
-  axios.post('/koalas', koalaObject).then((response) => {
-    clearForm();
-    //getKoalas();
-}).catch((error) => {
-    console.log('Error', error);
-    alert('Something went wrong');
-});
+function clearForm(){
+  document.getElementById('nameIn').value = '';
+  document.getElementById('ageIn').value = '';
+  document.getElementById('genderIn').value = '';
+  document.getElementById('readyForTransferIn').value = '';
+  document.getElementById('notesIn').value = '';
+}
+
+// function saveKoala(event){
+//   //event.preventDefault();
+//   console.log( 'in saveKoala' );
+//   // axios call to server to get koalas
+//   //made this an object to create request body
+//   let koalaObject ={
+//     name: document.querySelector('#nameIn').value,
+//     age: document.querySelector('#ageIn').value,
+//     gender: document.querySelector('#genderIn').value,
+//     readyForTransfer: document.querySelector('#readyForTransferIn').value,
+//     notes: document.querySelector('#notesIn').value
+//   }
+//   console.log(koalaObject);
+//   //send the new koala data to the server
+//   axios.post('/koalas', koalaObject).then((response) => {
+//     clearForm();
+//     getKoalas();
+// }).catch((error) => {
+//     console.log('Error', error);
+//     alert('Something went wrong');
+// });
  
+// }
+
+function saveKoala(koalaAdded) {
+const koalaToAdd= {
+  name: document.getElementById("nameIn").value,
+  age: document.getElementById("ageIn").value,
+  gender: document.getElementById("genderIn").value,
+  readyForTransfer: document.getElementById("readyForTransferIn").value,
+  notes: document.getElementById("notesIn").value
+}
+
+  axios({
+    method: "POST",
+    url: "/koalas",
+    data: koalaToAdd,
+  })
+    .then(function (response) {
+      console.log("saveKoala()", response.data);
+      getKoalas();
+      clearForm();
+    })
+    .catch(function (error) {
+      console.log("Error in POST", error);
+      alert("Unable to add koala at this time. Please try again later.");
+    });
+}
+
+function koalaReadyForTransfer(event) {
+
+  console.log('incoming event.target', event.target)
+  console.log('Getting dataset from component', event.target.closest("tr").dataset.id)
+
+  // Retrieving data that has been stored on an element
+  let bookId = event.target.closest("tr").dataset.id
+
+  axios.put(`/koalas/${koalaId}`)
+      .then((response) => {
+          getKoalas();
+      })
+      .catch((error) => {
+          console.log('Error', error);
+          alert('Something went wrong');
+      });
 }
 
 getKoalas();
+saveKoala()
